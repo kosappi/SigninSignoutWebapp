b5f971aafea278f7534d7e9745b948cda66a8975
kosappi <kosaka_ryuichi@b-engineer.com>
2017-06-24 19:26:08 +0900

    added password columns.

--- db/migrate/20170624071303_create_users.rb
+++ db/migrate/20170624071303_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :name
       t.string :mail
+      t.string :password_digest, null: false
 
       t.timestamps
     end
